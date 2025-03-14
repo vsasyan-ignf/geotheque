e705f32af599cd9c83e579e577fce6cef9c4e269
wulaurent <laurentwu123@gmail.com>
2025-03-14 11:03:08 +0100

    feat(map): Ajout d'une carte openlayers et d'une barre latérale à gauche avec intéraction

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,85 +1,131 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, nextTick } from "vue";
+
+const center = ref([40, 40]);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+
+const currentCenter = ref(center.value);
+const currentZoom = ref(zoom.value);
+const currentRotation = ref(rotation.value);
+const currentResolution = ref(0);
+
+const isSidebarOpen = ref(false);
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+}
+
+function resolutionChanged(event) {
+  currentResolution.value = event.target.getResolution();
+  currentZoom.value = event.target.getZoom();
+}
+
+function centerChanged(event) {
+  currentCenter.value = event.target.getCenter();
+}
+
+function rotationChanged(event) {
+  currentRotation.value = event.target.getRotation();
+}
+
+onMounted(() => {
+  // refresh le state pour le resize de la map
+  nextTick(() => {
+    window.dispatchEvent(new Event("resize"));
+  });
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<button
+    class="toggle-button"
+    :class="{ 'button-shift': isSidebarOpen }"
+    @click="toggleSidebar"
+  >    ☰
+  </button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div :class="['sidebar', { open: isSidebarOpen }]">
+    <h2>Ecureil Force</h2>
+    <p>Mon contenue info</p>
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="map-container" :class="{ 'map-shift': isSidebarOpen }">
+    <ol-map class="ol-map">
+      <ol-view
+        ref="view"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+        @change:center="centerChanged"
+        @change:resolution="resolutionChanged"
+        @change:rotation="rotationChanged"
+      />
+      <ol-tile-layer>
+        <ol-source-osm />
+      </ol-tile-layer>
+    </ol-map>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+.toggle-button {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  z-index: 10;
+  transition: transform 0.3s ease;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.toggle-button.button-shift {
+  transform: translateX(250px);
 }
 
-nav {
+.sidebar {
+  position: absolute;
+  top: 0;
+  left: -250px;
+  width: 250px;
+  height: 100vh;
+  background-color: #444;
+  color: white;
+  padding: 20px;
+  transition: left 0.3s ease;
+  z-index: 5;
+}
+
+.sidebar.open {
+  left: 0;
+}
+
+.map-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  transition: width 0.3s ease;
+}
+
+.map-container.map-shift {
+  width: calc(100vw);
+}
+
+.map-container:not(.map-shift) {
+  width: 100vw;
+}
+
+.ol-map {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  height: 100%;
 }
 </style>
+
