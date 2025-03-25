80ca39f77cb2d717e0d8b7ed3cea2ab7a875b235
Fuyutaru <zijian1105@gmail.com>
2025-03-25 11:19:35 +0100

    save

---
+++ app/src/components/composable/baseMap.js
@@ -0,0 +1,125 @@
+// export const tabs = [
+//     { id: 'carthotheque', icon: 'layers', title: 'Cartothèque France' },
+//     { id: 'carthotheque_etranger', icon: 'layers', title: 'Cartothèque Étranger' },
+//     { id: 'phototheque', icon: 'camera', title: 'Photothèque France' },
+//     { id: 'phototheque_etranger', icon: 'camera', title: 'Photothèque Étranger' },
+//     { id: 'aide', icon: 'lightbulb-on', title: 'Aide' },
+// ];
+
+// export const subCategories = [
+//     { id: 'commune', icon: 'city', title: 'Commune' },
+//     { id: 'departement', icon: 'map', title: 'Département' },
+//     { id: 'point', icon: 'crosshairs-gps', title: 'Point XY' },
+// ];
+
+export const layers_carto = ref([
+    {
+        id: 'plan',
+        name: 'Plan IGN',
+        thumbnail: PlanIGN,
+    },
+    {
+        id: 'ortho',
+        name: 'Ortho',
+        thumbnail: Ortho,
+    },
+    {
+        id: 'bdparcellaire',
+        name: 'BDParcellaire',
+        thumbnail: BDParcellaire,
+    },
+    {
+        id: 'cartesign',
+        name: 'Cartes IGN',
+        thumbnail: CartesIGN,
+    },
+    {
+        id: 'scan25',
+        name: 'Scan25',
+        thumbnail: Scan25,
+    },
+]);
+
+export const layers_carto_monde = ref([
+    {
+        id: 'plan',
+        name: 'Plan IGN',
+        thumbnail: PlanIGN,
+    },
+    {
+        id: 'ortho',
+        name: 'Ortho',
+        thumbnail: Ortho,
+    },
+    {
+        id: 'bdparcellaire',
+        name: 'BDParcellaire',
+        thumbnail: BDParcellaire,
+    },
+    {
+        id: 'cartesign',
+        name: 'Cartes IGN',
+        thumbnail: CartesIGN,
+    },
+    {
+        id: 'scan25',
+        name: 'Scan25',
+        thumbnail: Scan25,
+    },
+]);
+
+export const layers_photo = ref([
+    {
+        id: 'plan',
+        name: 'Plan IGN',
+        thumbnail: PlanIGN,
+    },
+    {
+        id: 'ortho',
+        name: 'Ortho',
+        thumbnail: Ortho,
+    },
+    {
+        id: 'bdparcellaire',
+        name: 'BDParcellaire',
+        thumbnail: BDParcellaire,
+    },
+    {
+        id: 'cartesign',
+        name: 'Cartes IGN',
+        thumbnail: CartesIGN,
+    },
+    {
+        id: 'scan25',
+        name: 'Scan25',
+        thumbnail: Scan25,
+    },
+]);
+
+export const layers_photo_monde = ref([
+    {
+        id: 'plan',
+        name: 'Plan IGN',
+        thumbnail: PlanIGN,
+    },
+    {
+        id: 'ortho',
+        name: 'Ortho',
+        thumbnail: Ortho,
+    },
+    {
+        id: 'bdparcellaire',
+        name: 'BDParcellaire',
+        thumbnail: BDParcellaire,
+    },
+    {
+        id: 'cartesign',
+        name: 'Cartes IGN',
+        thumbnail: CartesIGN,
+    },
+    {
+        id: 'scan25',
+        name: 'Scan25',
+        thumbnail: Scan25,
+    },
+]);
