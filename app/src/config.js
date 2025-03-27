027ac09bcaf57f088e1a198be8024d56b6533eff
BoThoma <thomas.boutonne@ensg.eu>
2025-03-27 15:36:15 +0100

    add some config

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,6 @@
 export default {
     basecommunesUrl: "https://geo.api.gouv.fr/communes",
     basedepartementsUrl: "https://geo.api.gouv.fr/departements",
-    baseGeoserverUrl: "http://localhost:8088/geoserver"
+    baseGeoserverUrl: "http://localhost:8088/geoserver",
+    baseNominatimUrl: "https://nominatim.openstreetmap.org"
 };
