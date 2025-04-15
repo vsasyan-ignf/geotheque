df8b2762dfd4cfbb4504b5bac151d6ab3eb23dca
wulaurent <laurentwu123@gmail.com>
2025-04-15 14:55:30 +0200

    feat: style personnalisé de la scrollbar

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -14,4 +14,32 @@ import MapBox from '@/components/MapBox.vue'
   width: 20px;
   color: #ffffff;
 }
+
+::-webkit-scrollbar {
+  width: 2px;
+  height: 2px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(100, 100, 100, 0.5);
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(115, 150, 20, 0.7);
+}
+
+* {
+  scrollbar-width: thin;
+  scrollbar-color: rgba(115, 150, 20, 0.7) transparent;
+}
+
+html, body {
+  scrollbar-width: thin;
+  scrollbar-color: rgba(115, 150, 20, 0.7) transparent;
+}
 </style>
